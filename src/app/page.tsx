2e3810b0fb9e9d3663cfd91b4703fe0da84af176
Jose Ripoll <jose.ripoll@acrismatic.es>
2023-09-05 08:33:46 +0200

    feat: newsletter-generator

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,142 @@
-import Image from 'next/image'
+"use client"
+
+import { printHTMLCode } from "@/lib/print-html-code";
+import { useState } from "react";
+
+
+function formatDate(inputDate: string) {
+  inputDate ? inputDate : inputDate = '2023/09/05';
+  const parts = inputDate.split('-');
+  const formattedDate = `${parts[2]}/${parts[1]}/${parts[0]}`;
+  return formattedDate;
+}
 
 export default function Home() {
+
+  const [match, setMatch] = useState<string>('')
+  const [date, setDate] = useState<string>()
+  const [backgroundUrl, setBackgroundUrl] = useState<string>('')
+  const [coutasUrl, setCuotasUrl] = useState<string>('')
+  const [landingPageUrl, setLandingPageUrl] = useState<string>('')
+
+  function copyHtml() {
+    navigator.clipboard.writeText(
+      printHTMLCode({
+        match,
+        date: formatDate(date!),
+        backgroundUrl,
+        coutasUrl,
+        landingPageUrl,
+      })
+    )
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="bg-neutral-900 min-h-screen">
+      <h1 className="font-bold text-2xl text-center text-neutral-300 mb-3">Newsletter - Juegging</h1>
+
+      <div className="w-11/12  md:w-9/12 lg:w-1/2 mx-auto bg-neutral-800/50 p-5 rounded" >
+        <form>
+          <fieldset className="flex gap-3">
+            <legend className="text-neutral-50 text-xl font-bold">Datos</legend>
+            <label htmlFor="match" className="block mb-5 w-full ">
+              <span className="text-neutral-400 text-xs">Partido</span>
+              <input className="border border-neutral-700 rounded my-2 p-2 w-full bg-neutral-600 text-neutral-50"
+                type="text"
+                name="match"
+                onChange={(event) => setMatch(event.target.value)}
+                placeholder="VALENCIA - LEVANTE"
+              />
+            </label>
+            <label htmlFor="date" className="block mb-5 w-full ">
+              <span className="text-neutral-400 text-xs">Fecha</span>
+              <input className="border border-neutral-700 rounded my-2 p-2 w-full bg-neutral-600 text-neutral-50"
+                type="date"
+                name="date"
+                onChange={(event) => setDate(event.target.value)}
+              />
+            </label>
+          </fieldset>
+
+
+          <fieldset className="flex items-center gap-3">
+            <legend className="text-neutral-50 text-xl font-bold ">Imágenes</legend>
+            <label htmlFor="backgroundUrl" className="block mb-5 w-full ">
+              <span className="text-neutral-400 text-xs">Background URL (Imágen)</span>
+              <input className="border border-neutral-700 rounded my-2 p-2 w-full bg-neutral-600 text-neutral-50"
+                type="text"
+                name="backgroundUrl"
+                onChange={(event) => setBackgroundUrl(event.target.value)}
+                placeholder="https://juegging.es/wp-content/newsletter/cashback-2023/valencia-levante/background.jpg"
+              />
+            </label>
+
+            <label htmlFor="coutasUrl" className="block mb-5 w-full ">
+              <span className="text-neutral-400 text-xs">Coutas URL (Imágen)</span>
+              <input className="border border-neutral-700 rounded my-2 p-2 w-full bg-neutral-600 text-neutral-50"
+                type="text"
+                name="coutasUrl"
+                onChange={(event) => setCuotasUrl(event.target.value)}
+                placeholder="https://juegging.es/wp-content/newsletter/cashback-2023/valencia-levante/coutas.jpg"
+              />
+            </label>
+
+          </fieldset>
+
+
+          <fieldset>
+            <legend className="text-neutral-50 text-xl font-bold">Enlaces</legend>
+            <label htmlFor="landingPageUrl" className="block mb-5">
+              <span className="text-neutral-400 text-xs">Landing page (URL)</span>
+              <input className="border border-neutral-700 rounded my-2 p-2 w-full bg-neutral-600 text-neutral-50"
+                type="text"
+                name="landingPageUrl"
+                onChange={(event) => setLandingPageUrl(event.target.value)}
+                placeholder="https://juegging.es/promocion/valencia-levante/"
+              />
+            </label>
+          </fieldset>
+
+
+
+        </form>
+      </div>
+
+
+      <section className="w-11/12  md:w-9/12 lg:w-1/2 mx-auto text-neutral-50">
+
+        <div className="flex items-center justify-start gap-9 mt-10 mb-3">
+          <h2 className="font-bold">Código HTML</h2>
+          <button className="bg-neutral-100 text-neutral-900 rounded px-2 py-1 hover:bg-neutral-200" onClick={copyHtml}>Copiar HTML</button>
         </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <details>
+          <summary className=" bg-neutral-100 text-neutral-900 rounded px-2 py-1 hover:bg-neutral-200 hover:cursor-pointer w-content">Mostrar código</summary>
+          <div className="bg-neutral-700 p-5 rounded" >
+            {
+              printHTMLCode({
+                match,
+                date: formatDate(date!),
+                backgroundUrl,
+                coutasUrl,
+                landingPageUrl,
+              })
+            }
+          </div>
+        </details>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        {/* <div className="bg-neutral-700 p-5 rounded" >
+          {
+            printHTMLCode({
+              match,
+              date: formatDate(date!),
+              backgroundUrl,
+              coutasUrl,
+              landingPageUrl,
+            })
+          }
+        </div> */}
+      </section>
     </main>
   )
 }
